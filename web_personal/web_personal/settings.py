9448d66cf58ea8d2bdcdac20a867a629913f9e8d
Edgar-JLF <60168763+Edgar-JLF@users.noreply.github.com>
2023-03-21 21:01:00 -0600

    Update email config

--- web_personal/web_personal/settings.py
+++ web_personal/web_personal/settings.py
@@ -147,7 +147,7 @@ CKEDITOR_CONFIGS = {
 #Email config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'edgarlopezf43@gmail.com'
-EMAIL_HOST_PASSWORD = 'xrctanthhtyyzyzu'
+EMAIL_HOST_USER = 'ejemplo@gmail.com'
+EMAIL_HOST_PASSWORD = 'adfadsfxadfahhtyyzyasdu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
